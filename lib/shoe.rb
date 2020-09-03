266c52b5ecab9ec0883a086860c612c44b04956b
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-03 02:42:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,16 @@
-class Shoe
-  def initialize=(brand)
-    @brand=brand
+class Book
+  
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  def initialize(title)
+    @title = title
   end
+  
+  def turn_page
+  puts "Flipping the page...wow, you read fast!"
 end
+
+end
+
+book = Book.new("Some Title")
+book.turn_page
